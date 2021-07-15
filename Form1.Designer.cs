4877268a7d7a08819e6366158e351969dc7a3cea
FL Developers <43763049+FLDevelopers@users.noreply.github.com>
2021-07-16 01:54:26 +0500

    v1.0

---
+++ Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace System_Sound_Recorder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.lbl_Timer = new System.Windows.Forms.Label();
+            this.Warning1 = new System.Windows.Forms.Label();
+            this.Warning2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(41, 114);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(100, 40);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Record";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Clicked);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(188, 114);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(100, 40);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = " Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Clicked);
+            // 
+            // Timer
+            // 
+            this.Timer.Enabled = true;
+            this.Timer.Interval = 1000;
+            this.Timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // lbl_Timer
+            // 
+            this.lbl_Timer.AutoSize = true;
+            this.lbl_Timer.Font = new System.Drawing.Font("Digital-7", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Timer.Location = new System.Drawing.Point(73, 35);
+            this.lbl_Timer.Name = "lbl_Timer";
+            this.lbl_Timer.Size = new System.Drawing.Size(170, 49);
+            this.lbl_Timer.TabIndex = 2;
+            this.lbl_Timer.Text = "00:00:00";
+            // 
+            // Warning1
+            // 
+            this.Warning1.AutoSize = true;
+            this.Warning1.Location = new System.Drawing.Point(40, 172);
+            this.Warning1.Name = "Warning1";
+            this.Warning1.Size = new System.Drawing.Size(254, 13);
+            this.Warning1.TabIndex = 3;
+            this.Warning1.Text = "*There must be come audio after pressing \"Record\".";
+            // 
+            // Warning2
+            // 
+            this.Warning2.AutoSize = true;
+            this.Warning2.Location = new System.Drawing.Point(61, 190);
+            this.Warning2.Name = "Warning2";
+            this.Warning2.Size = new System.Drawing.Size(207, 13);
+            this.Warning2.TabIndex = 4;
+            this.Warning2.Text = "*Choose output path before start recording";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(332, 215);
+            this.Controls.Add(this.Warning2);
+            this.Controls.Add(this.Warning1);
+            this.Controls.Add(this.lbl_Timer);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "System Sound Recorder";
+            this.Load += new System.EventHandler(this.Form_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Timer Timer;
+        private System.Windows.Forms.Label lbl_Timer;
+        private System.Windows.Forms.Label Warning1;
+        private System.Windows.Forms.Label Warning2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Windows.Forms;
+using NAudio.Wave;
+using System.IO;
+using System.Diagnostics;
+
+namespace System_Sound_Recorder
+{
+    public partial class Form1 : Form
+    {
+        Stopwatch stopwatch;
+        private string outputFileName;
+        private WasapiLoopbackCapture capture;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            stopwatch = new Stopwatch();
+        }
+
+        private void btnStart_Clicked(object sender, EventArgs e)
+        {
+            var dialog = new SaveFileDialog();
+            dialog.Filter = "wave files | *.wav";
+
+            if(dialog.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+
+            outputFileName = dialog.FileName;
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+
+            capture = new WasapiLoopbackCapture();
+            var writer = new WaveFileWriter(outputFileName, capture.WaveFormat);
+
+            capture.DataAvailable += async (s, t) =>
+            {
+                if (writer != null)
+                {
+                    await writer.WriteAsync(t.Buffer, 0, t.BytesRecorded);
+                    await writer.FlushAsync();
+                }
+            };
+
+            capture.RecordingStopped += (s, t) =>
+            {
+                if(writer != null)
+                {
+                    writer.Dispose();
+                    writer = null;
+                }
+
+                btnStart.Enabled = true;
+                btnStop.Enabled = false;
+                capture.Dispose();
+            };
+
+            capture.StartRecording();
+            stopwatch.Start();
+        }
+
+        private void btnStop_Clicked(object sender, EventArgs e)
+        {
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+            capture.StopRecording();
+            stopwatch.Stop();
+            stopwatch.Reset();
+
+            if(outputFileName == null)
+            {
+                return;
+            }
+
+            var processStartInfo = new ProcessStartInfo
+            {
+                FileName = Path.GetDirectoryName(outputFileName),
+                UseShellExecute = true
+            };
+
+            Process.Start(processStartInfo);
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            lbl_Timer.Text = string.Format("{0:hh\\:mm\\:ss}",stopwatch.Elapsed);
+        }
+    }
+}
